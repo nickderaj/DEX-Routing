a8f433f61287737bb734cc3dfdd262e17f836787
Nicholas De Raj <nickderaj@gmail.com>
2023-06-25 10:23:33 +0800

    build: add env variables

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const port = 8080;
+const port = Number(process.env.SERVER_PORT || 8080);
 app.listen(port, '0.0.0.0', () => {
   // eslint-disable-next-line no-console
   console.log(`Server listening on port ${port}!`);
